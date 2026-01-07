339db456fb7c039da421e2a420ba93530c3ca40e
Cергей Родионов <seregagepard777@gmail.com>
2026-01-07 15:24:19 +0300

    Первая версия

---
+++ Lesson_1/MyFirstApp.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("Написанно однажды, работает везде!");
+        int number = 42;
+        if(42 == 42) {
+            System.out.println(42);
+        Scanner console = new Scanner(System.in, "cp866");
+        System.out.print("Введите свое имя: ");
+        System.out.println(console.nextLine());
+        }
+    }
+}
---
+++ Lesson_1/MySecondApp.java
@@ -0,0 +1,19 @@
+public class MySecondApp {
+    public static void main(String[] args) {
+
+// просуммировать числа от 1 до 5
+    /*
+
+    1+2+3+4+5=15
+
+    */
+    
+    int sum = 1+2;
+    int sum1 = sum+3;
+    int sum2 = sum1+4;
+    int sum3 = sum2+5;
+    System.out.println(sum3);
+
+    
+    }
+}
