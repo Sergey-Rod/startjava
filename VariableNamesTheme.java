c6a6596ac96ef196be85125d65ae13902ab80cc9
Cергей Родионов <seregagepard777@gmail.com>
2026-01-09 19:29:35 +0300

    Первое задание из раздела Переменые

---
+++ VariableNamesTheme.java
@@ -0,0 +1,196 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+        // Блок 1. РАЗНЫЕ ПЕРЕМЕННЫЕ
+        System.out.println("1. РАЗНЫЕ ПЕРЕМЕННЫЕ");
+        System.out.println();   
+    
+         // Цифра
+        int digit = 9;
+        System.out.println("Цифра = " + digit);
+
+        // Сумма чисел
+        int sum = 76 + 22;
+        System.out.println("Сумма чисел = " + sum);
+
+        // Произведение цифр
+        int product = 12 * 12;
+        System.out.println("Произведение цифр = " + product);
+
+        // Максимальное число
+        int maxNumber = 10000;
+        System.out.println("Максимальное число = " + maxNumber);
+
+        // Количество десятков
+        int tensCount = 7;
+        System.out.println("Количество десятков = " + tensCount);
+
+        // Вес собаки
+        double dogWeight = 12.5;
+        System.out.println("Вес собаки = " + dogWeight + " кг");
+
+        // Исходное число
+        int originalNumber = 965;
+        System.out.println("Исходное число = " + originalNumber);
+
+        // Процент по вкладу
+        double interestRate = 33.3;
+        System.out.println("Процент по вкладу = " + interestRate + "%");
+
+        // Символ &
+        char ampersand = '&';
+        System.out.println("Символ & = " + ampersand);
+
+        // Код ошибки
+        int errorCode = 404;
+        System.out.println("Код ошибки = " + errorCode);
+
+        // Тип сообщения
+        String messageType = "Главный_тип";
+        System.out.println("Тип сообщения = " + messageType);
+
+        // Число нулей
+        int zeroCount = 7;
+        System.out.println("Число нулей = " + zeroCount);
+
+        // Уникальное число
+        double uniqueNumber = 98765.4321;
+        System.out.println("Уникальное число = " + uniqueNumber);
+
+        // Случайное число
+        double randomNumber = 42.767;
+        System.out.println("Случайное число = " + randomNumber);
+
+        // Математическое выражение
+        double mathExpression = (5.65 + 3.21) * 2;
+        System.out.println("Математическое выражение = " + mathExpression);
+
+        // Счёт в игре
+        int gameScore = 5678;
+        System.out.println("Счёт в игре = " + gameScore);
+
+        // Максимальная длина
+        double maxLength = 120.76;
+        System.out.println("Максимальная длина = " + maxLength);
+
+        // Пункт меню
+        int menuItem = 65;
+        System.out.println("Пункт меню = " + menuItem);
+
+        // Стоимость кофе
+        double coffeePrice = 123.55;
+        System.out.println("Стоимость кофе = " + coffeePrice + " руб.");
+
+        // Начальная дата
+        int startDate = 20260109;
+        System.out.println("Начальная дата = " + startDate);
+
+        // Окончание диапазона
+        double endRange = 100.00;
+        System.out.println("Окончание диапазона = " + endRange);
+
+        // Имя работника месяца
+        String employeeOfMonth = "Большаков Александр";
+        System.out.println("Имя работника месяца = " + employeeOfMonth);
+
+        // Название электронной книги
+        String ebookTitle = "Все числа по возрастанию";
+        System.out.println("Название электронной книги = " + ebookTitle);
+
+        // Размер
+        double size = 43.5;
+        System.out.println("Размер = " + size);
+
+        // Вместимость
+        double capacity = 50.05;
+        System.out.println("Вместимость = " + capacity);
+
+        // Счетчик
+        int counter = 20;
+        System.out.println("Счётчик = " + counter);
+
+        // Путь до файла
+        String filePath = "D:\\JAVA\\STARTJAVA";
+        System.out.println("Путь до файла = " + filePath);
+
+        // Количество чисел в строке
+        int numbersInLine = 14;
+        System.out.println("Количество чисел в строке = " + numbersInLine);
+
+        System.out.println();
+
+        // Блок 2. BOOLEAN-ПЕРЕМЕННЫЕ
+        System.out.println("2. BOOLEAN-ПЕРЕМЕННЫЕ");
+        System.out.println();
+
+        // Сотни равны?
+        boolean hundredsEqual = false;
+        System.out.println("Сотни равны? = " + hundredsEqual);
+
+        // Компьютер включён?
+        boolean isComputerOn = true;
+        System.out.println("Компьютер включён? = " + isComputerOn);
+
+        // Есть равные цифры?
+        boolean hasEqualDigits = true;
+        System.out.println("Есть равные цифры? = " + hasEqualDigits);
+
+        // Служба создана?
+        boolean isServiceCreated = false;
+        System.out.println("Служба создана? = " + isServiceCreated);
+
+        // Файл пустой?
+        boolean isFileEmpty = false;
+        System.out.println("Файл пустой? = " + isFileEmpty);
+
+        // Соединение активное?
+        boolean isConnectionActive = true;
+        System.out.println("Соединение активное? = " + isConnectionActive);
+
+        // Новый?
+        boolean isNew = true;
+        System.out.println("Новый? = " + isNew);
+
+        // Электронная почта действительная?
+        boolean isEmailValid = true;
+        System.out.println("Электронная почта действительная? = " + isEmailValid);
+
+        // Имеются уникальные строки?
+        boolean hasUniqueStrings = false;
+        System.out.println("Имеются уникальные строки? = " + hasUniqueStrings);
+
+        System.out.println();
+
+        // Блок 3. АББРЕВИАТУРЫ
+        System.out.println("3. АББРЕВИАТУРЫ");
+        System.out.println();
+
+        // Старый universally unique identifier
+        String oldUUID = "123e4567-e89b-12d3-a456-426614174000";
+        System.out.println("Старый UUID = " + oldUUID);
+
+        // Производитель оперативной памяти
+        String ramManufacturer = "Kingston";
+        System.out.println("Производитель оперативной памяти = " + ramManufacturer);
+
+        // Ёмкость жёсткого диска
+        int hddCapacity = 2;
+        System.out.println("Ёмкость жёсткого диска = " + hddCapacity + " ТБ");
+
+        // Определение термина протокола передачи гипертекста
+        String httpDefinition = "HyperText Transfer Protocol";
+        System.out.println("Определение термина протокола передачи гипертекста = " + httpDefinition);
+
+        // Сокращённый uniform resource locator
+        String shortURL = "bit.ly/12345";
+        System.out.println("Сокращённый URL = " + shortURL);
+
+        // Новый идентификатор клиента
+        String newClientId = "CLT-2026-001";
+        System.out.println("Новый идентификатор клиента = " + newClientId);
+
+        // Количество символов в american standard code for information interchange
+        int asciiCharsCount = 128;
+        System.out.println("Количество символов в ASCII = " + asciiCharsCount);
+
+    }
+}
